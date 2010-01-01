1402f3850269191c376d69115318bc07c01ed577
unknown <Jen@.(none)>
2010-01-01 10:37:02 -0600

    Added phpDoc comments. Adjusted assign() to accept array for key

--- libraries/Template.php
+++ libraries/Template.php
@@ -3,6 +3,15 @@
 
 require "smarty/Smarty.class.php";
 
+/**
+ * Template
+ * 
+ * @package   
+ * @author nerdmom
+ * @copyright nerdmom
+ * @version 2010
+ * @access public
+ */
 class Template extends Smarty {
 
      /* This is the theme that will load by default, unless you
@@ -10,6 +19,11 @@ class Template extends Smarty {
      */
     var $theme = "default";
 
+    /**
+     * Template::__construct()
+     * 
+     * @return
+     */
     function __construct()
     {
           // Define some Smarty paths
@@ -20,24 +34,58 @@ class Template extends Smarty {
         $this->tpl->plugins_dir = APPPATH . "libraries/smarty/plugins/";
     }
 
-     /**
-     *
+    /**
+     * Template::assign()
+     * Assigns values to be parsed by Smarty
+     * If key is an array, we can loop through to assign multiple values at once
+     * @param mixed $key - can be associative array
+     * @param mixed $value - optional unless key is a string
+     * @return
      */
-    function assign($key,$value)
+    function assign($key, $value = "")
     {
-        $this->tpl->assign($key,$value);
+    		if( is_array($key) ) {
+    			foreach($key as $var => $val) {
+    				$this->tpl->assign($var, $val);    			
+    			}    		    		    		    		
+    		} else {
+    			if( empty($value) ) {
+    				throw new Exception("Value must be set when key is a string!");    			
+    			}
+			    
+			$this->tpl->assign($key,$value);    		    		    		    		
+    		}        
     }
 
+    /**
+     * Template::display()
+     * 
+     * @param mixed $template
+     * @return
+     */
     function display($template)
     {
         $this->tpl->display($template);
     }
 
+    /**
+     * Template::fetch()
+     * 
+     * @param mixed $template
+     * @return
+     */
     function fetch($template)
     {
         $this->tpl->fetch($template);
     }
 
+    /**
+     * Template::layout()
+     * 
+     * @param mixed $inner_template
+     * @param mixed $array
+     * @return
+     */
     function layout($inner_template,$array)
     {
           foreach($array as $key=>$val) {
